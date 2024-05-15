ec389e2a8825660ac82a6ad51648cfc3cec41712
Joseph <59439026+JosephKBS@users.noreply.github.com>
2024-05-15 10:48:40 -0400

    upoload

--- hm.py
+++ hm.py
@@ -3,17 +3,9 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import gc
-import sys
-import sas7bdat
-from sas7bdat import SAS7BDAT
 import time
-import pathlib
-import os.path
 import pyarrow as pa
 import pyarrow.parquet as pq
-#!pip install pgeocode
-import pgeocode
-import step1
 import time
 #!pip uninstall -y splink
 #!pip install splink
@@ -21,47 +13,13 @@ import splink
 from splink.duckdb.duckdb_linker import DuckDBLinker
 import splink.duckdb.duckdb_comparison_library as cl
 import splink.duckdb.comparison_template_library as ctl
-
-blocking_rule_prov = [
-    "l.DOB = r.DOB and l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.ln=r.ln and l.sex=r.sex and l.ssn=r.ssn",
-    "l.DOB = r.DOB and l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.ln=r.ln and l.ssn=r.ssn",
-    "l.DOB = r.DOB and l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.ln=r.ln and l.sex=r.sex",
-    "l.DOB = r.DOB and l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.sex=r.sex and l.ssn=r.ssn",
-    "l.DOB = r.DOB and l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.sex=r.sex and l.ssn=r.ssn",
-    "l.DOB = r.DOB and l.sex=r.sex and l.ln=r.ln and l.ssn=r.ssn",
-    "l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.sex=r.sex and l.ln=r.ln and l.ssn=r.ssn",
-    "l.DOB = r.DOB and l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.ssn=r.ssn",
-    "l.DOB = r.DOB and l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.ln=r.ln",
-    "l.DOB = r.DOB and l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.sex=r.sex",
-    "l.DOB = r.DOB and l.ssn=r.ssn and l.sex=r.sex",
-    "l.DOB = r.DOB and l.ssn=r.ssn and l.ln=r.ln",
-    "l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.ln=r.ln and l.ssn=r.ssn",
-    "l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.ln=r.ln and l.sex=r.sex",
-    "l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.sex=r.sex and l.ssn=r.ssn",
-    "l.PROVIDER_NUMBER=r.PROVIDER_NUMBER and l.sex=r.sex and l.ln=r.ln",
-    "l.DOB = r.DOB and l.ssn=r.ssn",
-    "l.DOB = r.DOB"
-]
-
-
-testa = pd.DataFrame({
-    'sex':[1,2,1,2,1,2,1,2,1,2],
-    'dob':['2012-1-1','2011-12-1','1999-1-1','1998-11-1','2012-11-1','1984-1-1','1982-1-1','1975-1-1','1967-1-1','1954-1-1'],
-    'ssn':[1111,2222,3333,4444,5555,6666,7777,8888,9999,1010],
-    'ln':["as",'ss','zz','rr','ww','wa','tr','tt','hh','gq'],
-    'PROVIDER_NUMBER':[2,1,1,1,1,1,1,1,2,1]
-})
-testb = pd.DataFrame({
-    'sex':[2,2,1,1,1,2,1,2,1,1],
-    'dob':['2012-1-1','2001-12-1','1999-1-1','1998-11-1','2012-11-1','1984-1-1','1982-1-1','1975-1-1','1967-1-1','1954-1-1'],
-    'ssn':[1111,2222,3333,4444,5555,6666,7777,8888,9999,1010],
-    'ln':["as",'ls','zz','rr','wb','wa','tr','tt','ha','gq'],
-    'PROVIDER_NUMBER':[2,1,1,1,1,1,1,1,2,1]
-})
-
+from blocking_rule import (blocking_rule, testa, testb)
 
    
-def hm(df_a=testa, df_b=testb, col_a=None, col_b=None,
+def hm(df_a, 
+       df_b, 
+       col_a=None, 
+       col_b=None,
        match_prob_threshold=0.001, 
        iteration=20,
        blocking_rule_prov = blocking_rule_prov,
@@ -73,10 +31,20 @@ def hm(df_a=testa, df_b=testb, col_a=None, col_b=None,
        visual_waterfall=False,
       match_summary=False,
       ):
-    blocking_rule_for_training = f'l.{blocking_rule_for_training_input} = r.{blocking_rule_for_training_input}'
+    if df_a.empty :
+        raise ValueError("Left dataframe is empty")
+    elif df_b.empty:
+        raise ValueError("Right dataframe is empty")
+    if not blocking_rule_for_training_input in df_a or not blocking_rule_for_training_input in df_b:
+        raise ValueError("Missing blocking columns in data!")
+    else:
+        blocking_rule_for_training = f'l.{blocking_rule_for_training_input} = r.{blocking_rule_for_training_input}'
+    
     test1 = healmatcher(
-        df_a=testa, df_b=testb, 
-        col_a=col_a, col_b=col_b, 
+        df_a=testa, 
+        df_b=testb, 
+        col_a=col_a, 
+        col_b=col_b, 
         match_prob_threshold=match_prob_threshold, 
         iteration=iteration_input 
     )
@@ -96,11 +64,16 @@ def hm(df_a=testa, df_b=testb, col_a=None, col_b=None,
                               iteration=iteration, 
                               blocking_rule_prov = blocking_rule_prov
     )
-    model1=test1.model_training(linker = linker, blocking_rule_prov = blocking_rule_prov,
-        blocking_rule_for_training = blocking_rule_for_training, match_prob_threshold = match_prob_threshold
+    model1=test1.model_training(linker = linker, 
+                                blocking_rule_prov = blocking_rule_prov,
+                                blocking_rule_for_training = blocking_rule_for_training, 
+                                match_prob_threshold = match_prob_threshold
     )
     try:
-        test1.model_visual( df_predictions=model1[0], linker=model1[1], visual_matchweight=True, visual_waterfall=True)
+        test1.model_visual( df_predictions=model1[0], 
+                           linker=model1[1], 
+                           visual_matchweight=True, 
+                           visual_waterfall=True)
     except:
         print("Visualization error")
         pass
@@ -110,7 +83,7 @@ def hm(df_a=testa, df_b=testb, col_a=None, col_b=None,
         un_a=test1.find_unmatch(data_cds1, model1[0].as_pandas_dataframe())
         un_b=test1.find_unmatch(data_medicaid1, model1[0].as_pandas_dataframe())
         if un_a.shape[0]>0 and un_b.shape[0]>0:
-            linker2 = test1.model_setup( df_a=un_a,
+            linker2 = test1.model_setup(df_a=un_a,
                                         df_b=un_b, 
                                         col_a=col_a,
                                         col_b=col_b, 
@@ -141,7 +114,7 @@ def hm(df_a=testa, df_b=testb, col_a=None, col_b=None,
     gc.collect()
     if match_summary == True:
         print("-"*10,"Summary","-"*10)
-        print("Matched trackid counts: ",df1['unique_id_r'].nunique())
+        print("Matched trackid counts: ", df1['unique_id_r'].nunique())
         print("Matched rate (cds): ", round(df1['unique_id_r'].nunique()/data_cds1['trackid'].nunique(),2 )*100,"%" )
         print("Matched rate (Medicaid): ", round(df1['unique_id_r'].nunique()/data_medicaid1['trackid'].nunique(),2 )*100,'%' )
     print("Matching complete")
@@ -156,7 +129,9 @@ class healmatcher:
                  blocking_rule = blocking_rule_prov,
                  blocking_rule_for_training = "l.PROVIDER_NUMBER=r.PROVIDER_NUMBER",
                  match_prob_threshold=0.001, 
-                 iteration=20):
+                 iteration=20,
+                 data_name = ['data_left','data_right']
+                 ):
         self.df_a = df_a
         self.df_b = df_b
         self.col_a = col_a
@@ -165,6 +140,7 @@ class healmatcher:
         self.iteration = iteration
         self.blocking_rule_for_training = blocking_rule_for_training
         self.blocking_rule = blocking_rule
+        self.data_name = data_name
     
     def trackid_gen(self, check3):
         check3 = check3.astype({'sex':str,'ssn':str })
@@ -188,24 +164,34 @@ class healmatcher:
             df_a['dob'] = pd.to_datetime(df_a['dob'])
         return df_a
     
-    def model_setup(self, df_a, df_b, col_a, col_b, variable, model_status, match_prob_threshold, iteration, blocking_rule_prov):
+    def model_setup(self, 
+                    df_a, df_b, 
+                    col_a, col_b, 
+                    variable, 
+                    model_status, 
+                    match_prob_threshold, 
+                    iteration, 
+                    blocking_rule_prov,
+                    data_name):
         if model_status == 1:
             settings = {
                 "link_type": "link_only",
                 "blocking_rules_to_generate_predictions":  blocking_rule_prov,
                 "comparisons": [            
-                    ctl.date_comparison( variable[0],
+                    ctl.date_comparison( variable[variable.index('dob')],
                         levenshtein_thresholds=[0.9],
                         datediff_thresholds=[30, 12, 1],
                         datediff_metrics=["day", "month", "year"],
                     ),
-                    ctl.name_comparison(variable[1],
+                    ctl.name_comparison(variable[variable.index('ln')],
                             levenshtein_thresholds=[1],
                             jaro_winkler_thresholds=[],
                             jaccard_thresholds=[1]
                     ),
-                    cl.levenshtein_at_thresholds(variable[2], term_frequency_adjustments=True),
-                    cl.levenshtein_at_thresholds(variable[3], term_frequency_adjustments=True)
+                    cl.levenshtein_at_thresholds(variable[variable.index('ssn')], 
+                                                 term_frequency_adjustments=True),
+                    cl.levenshtein_at_thresholds(variable[variable.index('sex')], 
+                                                 term_frequency_adjustments=True)
                 ],
                 "retain_matching_columns": True,
                 "retain_intermediate_calculation_columns": True,
@@ -217,17 +203,18 @@ class healmatcher:
                 "link_type": "link_only",
                 "blocking_rules_to_generate_predictions":  blocking_rule_prov,
                 "comparisons": [            
-                    ctl.date_comparison(variable[0],
+                    ctl.date_comparison(variable[variable.index('dob')],
                         levenshtein_thresholds=[0.9],
                         datediff_thresholds=[30, 12, 1],
                         datediff_metrics=["day", "month", "year"],
                     ),
-                    ctl.name_comparison(variable[1],
+                    ctl.name_comparison(variable[variable.index('ln')],
                             levenshtein_thresholds=[1],
                             jaro_winkler_thresholds=[],
                             jaccard_thresholds=[1]
                     ),
-                    cl.levenshtein_at_thresholds(variable[3], term_frequency_adjustments=True)
+                    cl.levenshtein_at_thresholds(variable[variable.index('sex')], 
+                                                 term_frequency_adjustments=True)
                 ],
                 "retain_matching_columns": True,
                 "retain_intermediate_calculation_columns": True,
@@ -236,19 +223,28 @@ class healmatcher:
             }            
         linker = DuckDBLinker([df_a,df_b],
                               settings, 
-                              input_table_aliases=["CDS", "MEDICAID"] )
+                              input_table_aliases= data_name )
         return linker
     
-    def model_training(self, linker, blocking_rule_prov, blocking_rule_for_training, match_prob_threshold):
+    def model_training(self, 
+                       linker, 
+                       blocking_rule_prov, 
+                       blocking_rule_for_training, 
+                       match_prob_threshold):
         linker.estimate_probability_two_random_records_match( blocking_rule_prov, recall=0.6 )
+        gc.collect()
         linker.estimate_u_using_random_sampling(max_pairs=1e7)    
         gc.collect()
         linker.estimate_parameters_using_expectation_maximisation(blocking_rule_for_training)
         gc.collect()
         df_predictions = linker.predict(threshold_match_probability= match_prob_threshold)
-        return [df_predictions,linker]#.as_pandas_dataframe()
+        return [df_predictions,linker]
 
-    def model_visual(self, df_predictions, linker, visual_matchweight=True, visual_waterfall=True):
+    def model_visual(self, 
+                     df_predictions, 
+                     linker, 
+                     visual_matchweight=True, 
+                     visual_waterfall=True):
         if visual_matchweight==True:
             display(linker.match_weights_chart())
         if visual_waterfall == True:
