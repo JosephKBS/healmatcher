61d69bd7ab5fe71aae2515c0456330a71096b2ff
Joseph <59439026+JosephKBS@users.noreply.github.com>
2024-05-15 15:53:20 -0400

    Upload

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 # test
 import pandas as pd
 import numpy as np
-from blocking_rule import (blocking_rule_prov,  testa, testb)
 from healmatcher import hm
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.11' 
+VERSION = '0.0.12' 
 DESCRIPTION = 'Fast and simple probabilistic data matching package'
 
 # Setting up
