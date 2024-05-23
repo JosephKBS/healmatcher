b194736afec453caf3aacae37763de55eb2d5f40
Joseph <59439026+JosephKBS@users.noreply.github.com>
2024-05-23 13:16:05 -0400

    Upload

--- hm/hm.py
+++ hm/hm.py
@@ -11,7 +11,6 @@ from splink.duckdb.duckdb_linker import DuckDBLinker
 import splink.duckdb.duckdb_comparison_library as cl
 import splink.duckdb.comparison_template_library as ctl
 from IPython.display import display
-from blocking_rule import *
 from . import blocking_rule_prov
 
 
